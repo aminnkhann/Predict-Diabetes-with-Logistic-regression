9693457360efa948352c5b61e87c995c75e95d5d
aminnkhann <heidarkhanzade@gmail.com>
2026-07-02 15:57:45 +0200

    feat: add train test split helpers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd  # Import pandas so we can load and work with the CSV data as a DataFrame.
 from pathlib import Path  # Import Path so the file path works reliably from this project folder.
-
+from sklearn.model_selection import train_test_split
 
 # Create the path to the folder where the dataset is stored.
 DATA_DIR = Path(__file__).parent.resolve() / "data"
@@ -9,3 +9,11 @@ DATA_DIR = Path(__file__).parent.resolve() / "data"
 def load_data():
     return pd.read_csv(DATA_DIR / "diabetes.csv")
 
+# Create X, which contains only the input features used to make predictions.
+# Create y, which contains the target answer that the model should learn to predict.
+def features_and_target(df):
+    return df.drop("Outcome", axis=1), df["Outcome"]
+
+# Split the data into training and testing sets.
+def split_data(X, y):
+        return train_test_split(X, y, test_size=0.25, random_state=42)
